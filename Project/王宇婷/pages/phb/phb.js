cec3e99da048991add9437e5643bf592314cd7e0
王宇婷 <44824987+wyttttt@users.noreply.github.com>
2020-12-05 21:21:20 +0800

    Add files via upload

---
+++ Project/王宇婷/pages/phb/phb.js
@@ -0,0 +1,89 @@
+// pages/phb/phb.js
+Page({
+
+    /**
+     * 页面的初始数据
+     */
+    data: {
+
+    },
+    goTojdb:function(f){
+        wx.navigateTo({
+          url: '/pages/jdb/jdb',
+          })
+      },
+    goTophb:function(f){
+        wx.navigateTo({ 
+          url: '/pages/phb/phb',
+          })
+      },
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function () {
+      var that = this
+      wx.request({
+        url:'https://www.responsibility.pro:2223/highprise',
+        headers:{ 
+          'Content-Type':'application/json'
+        },
+        success:function(res){
+          that.setData({
+            content:res.data,
+          })
+        },
+        fail:function(err){
+          console.log(err)
+        }
+      })
+    },
+
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function () {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function () {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function () {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function () {
+
+    }
+})
