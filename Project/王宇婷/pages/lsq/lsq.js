74ce85f5e126f9be10d63ed22b7ef7e7eaedccaa
王宇婷 <44824987+wyttttt@users.noreply.github.com>
2020-12-05 21:20:23 +0800

    Add files via upload

---
+++ Project/王宇婷/pages/lsq/lsq.js
@@ -0,0 +1,91 @@
+// pages/lsq/lsq.js
+Page({
+
+    /**
+     * 页面的初始数据
+     */
+    data: {
+
+    },
+    goTobianji:function(f){
+        wx.navigateTo({
+          url: '/pages/bianji/bianji',
+          })
+      },
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function (options) {
+      var that = this
+      wx.request({
+        url:'https://www.responsibility.pro:2347/lawyer_circle',
+        method:'get',
+        headers:{ 
+          'Content-Type':'application/json'
+        },
+        success:function(res){
+          console.log(res)
+          that.setData({
+            lawyer_circle:res.data,
+          })
+        },
+        fail:function(err){
+          console.log(err)
+        }
+      })
+    },
+
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function () {
+      this.setData({
+            content1:decodeURIComponent(wx.getStorageSync('content1')),
+            content2:decodeURIComponent(wx.getStorageSync('content2'))
+        });
+        console.log('已刷新')
+        this.onLoad()
+    },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function () {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function () {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function () {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function () {
+
+    }
+})
