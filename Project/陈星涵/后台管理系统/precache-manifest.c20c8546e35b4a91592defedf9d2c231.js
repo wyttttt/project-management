40b1c706e4bbdae8f0d46961488afca432f43593
chenxinghann <45022658+chenxinghann@users.noreply.github.com>
2020-12-16 14:59:04 +0800

    Add files via upload

---
+++ Project/陈星涵/后台管理系统/precache-manifest.c20c8546e35b4a91592defedf9d2c231.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be7eb728a43a0f11519b7e9e9ace254f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fc4b0244aa7bf1cf24c3",
+    "url": "/static/css/main.c1c342f0.chunk.css"
+  },
+  {
+    "revision": "365a90357bc21d854867",
+    "url": "/static/js/2.45867a0a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.45867a0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc4b0244aa7bf1cf24c3",
+    "url": "/static/js/main.cd30d278.chunk.js"
+  },
+  {
+    "revision": "4077d3fc6d7b163a2ab3",
+    "url": "/static/js/runtime-main.537f6dec.js"
+  },
+  {
+    "revision": "98de4bca51ad13d83f83b89b4d62a26a",
+    "url": "/static/media/bg.98de4bca.png"
+  }
+]);
